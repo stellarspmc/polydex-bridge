fc747f84baf4aca18c39ef52189e5b78a76851c3
tcfplayz <chau123ka89chun10p@gmail.com>
2026-08-10 03:10:02 +0800

    port: 26.2

--- src/main/java/dev/mattidragon/polydexbridge/PolydexRecipesPacket.java
+++ src/main/java/dev/mattidragon/polydexbridge/PolydexRecipesPacket.java
@@ -2,25 +2,26 @@ package dev.mattidragon.polydexbridge;
 
 import dev.mattidragon.polydexbridge.data.BridgeRecipe;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
-import net.minecraft.network.RegistryByteBuf;
-import net.minecraft.network.codec.PacketCodec;
-import net.minecraft.network.codec.PacketCodecs;
-import net.minecraft.network.packet.CustomPayload;
-import net.minecraft.util.Identifier;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.minecraft.resources.Identifier;
+import org.jspecify.annotations.NonNull;
 
 import java.util.List;
 
-public record PolydexRecipesPacket(List<BridgeRecipe> recipes) implements CustomPayload {
-    public static final PacketCodec<RegistryByteBuf, PolydexRecipesPacket> CODEC 
-            = PacketCodec.tuple(BridgeRecipe.CODEC.collect(PacketCodecs.toList()), PolydexRecipesPacket::recipes, PolydexRecipesPacket::new);
-    public static final Id<PolydexRecipesPacket> ID = new Id<>(Identifier.of(PolydexBridge.MOD_ID, "sync_recipes"));
+public record PolydexRecipesPacket(List<BridgeRecipe> recipes) implements CustomPacketPayload {
+    public static final StreamCodec<RegistryFriendlyByteBuf, PolydexRecipesPacket> CODEC
+            = StreamCodec.composite(BridgeRecipe.CODEC.apply(ByteBufCodecs.list()), PolydexRecipesPacket::recipes, PolydexRecipesPacket::new);
+    public static final Type<PolydexRecipesPacket> ID = new Type<>(Identifier.fromNamespaceAndPath(PolydexBridge.MOD_ID, "sync_recipes"));
     
     public static void register() {
-        PayloadTypeRegistry.playS2C().register(ID, CODEC);
+        PayloadTypeRegistry.serverboundPlay().register(ID, CODEC);
     }
     
     @Override
-    public Id<? extends CustomPayload> getId() {
+    public @NonNull Type<? extends CustomPacketPayload> type() {
         return ID;
     }
 }
